8b694b15562aecf10a3492e34be0d9115791d35b
Shelby Polasek <shelbypolasek@gmail.com>
2020-04-23 17:46:23 -0500

    function drills bonus work

--- js/function-drills-bonus.js
+++ js/function-drills-bonus.js
@@ -523,22 +523,45 @@
 //=======================================================================================
     // Write a function called add(a, b) that returns the sum of a and b
     function add(a, b) {
-        return
+        return a+b;
     }
     //=======================================================================================
     // Write a function called subtract(a, b) that return the difference between the two inputs.
+    function subtract(a, b) {
+        return a-b;
+    }
     //=======================================================================================
     // Write multiply(a, b) function that returns the product
+    function multiply(a, b) {
+        return a * b;
+    }
     //=======================================================================================
     // Write a divide(numerator, denominator) function that returns a divided by b
-    //=======================================================================================
+        function divide(numerator, denominator) {
+            return numerator / denominator;
+        }
+        //=======================================================================================
     // Write a remainder(number, divisor) function that returns the remainder left over when dividing number by the divisor
-    //=======================================================================================
+        function remainder(num1, num2) {
+            return num1 % num2;
+        }
+//=======================================================================================
     // Write the function square(a) that takes in a number and returns the number multiplied by itself.
-    //=======================================================================================
+            function square(a) {
+                return square(a);
+            }
+//=======================================================================================
     // Write a function called sumOfSquares(a, b) that uses only your add() function and your square function and not + or * operators
-    //=======================================================================================
+            function sumOfSquares(a, b) {
+                return add((square(a)),square(b));
+            }
+            //=======================================================================================
     // Write a function called doMath(operator, a, b) that takes 3 parameters. The first parameter is the name of the math function you want to apply. a and b are the two numbers to run that function on.
+            function doMath(operator, a, b) {
+                return operator(a, b);
+            }
+
+    // console.log(doMath(sum((3, 2))));
     //=======================================================================================
 
 
