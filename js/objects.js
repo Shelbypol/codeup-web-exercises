7c06b9c36d8f7ae9c33d4cd6691265f82849969d
Shelby Polasek <shelbypolasek@gmail.com>
2020-05-01 17:01:04 -0500

    finish working through last problem

--- js/objects.js
+++ js/objects.js
@@ -21,6 +21,14 @@
      * Example
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
+        var person = {
+            first: 'Shelby',
+            last: 'Polasek',
+            sayHello: function () {
+                console.log('Hello from ' + person.first + ' ' + person.last);
+            }
+        };
+        // console.log(person.sayHello());
 
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
@@ -36,11 +44,27 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
+
+    shoppers.forEach(function(shopper){
+        if(shopper.amount > 200){
+            var originalAmount = (shopper.amount).toFixed(2);
+            var discountPercent = .12;
+            var discountAmount = (originalAmount - (originalAmount * discountPercent)).toFixed(2);
+            var name = shopper.name;
+            console.log(name + ' your amount before discount is $' + originalAmount + '. \n' + 'Your discount is ' + discountPercent * 100 + '%' + '. \n' + 'Your final total after the discount is $' + discountAmount);
+        } else if (shopper.amount < 200){
+            var originalAmount = (shopper.amount).toFixed(2);
+            var discountPercent = 0;
+            var discountAmount = (originalAmount - (originalAmount * discountPercent)).toFixed(2);
+            var name = shopper.name;
+            console.log(name + ' your amount before discount is $' + originalAmount + '. \n' + 'Your discount is ' + discountPercent * 100 + '%' + '. \n' + 'Your final total after the discount is $' + discountAmount);
+        }
+    });
 
     /** TODO:
      * Create an array of objects that represent books and store it in a
@@ -55,6 +79,54 @@
      * > console.log(books[0].author.lastName) // "Adams"
      */
 
+    var books = [
+        {
+          title: 'All My Friends Are Dead',
+          author: {
+              firstName: 'Avery',
+              lastName: 'Monsen'
+          },
+        },
+        {
+         title: 'Burned My Hand',
+        author: {
+             firstName: 'Mitt',
+             lastName: 'Ovn'
+        }
+        },
+        {
+        title: 'Did You Hear That',
+        author:{
+            firstName: 'Mr',
+            lastName: 'Huh'
+        }
+        },
+        {
+        title: 'Hello',
+        author: {
+            firstName:'C',
+            lastName: 'Ya'
+        }
+        },
+        {
+       title: 'No',
+       author:{
+           firstName: 'Ms.Computer',
+           lastName: 'Says'
+       }
+        },
+    ];
+
+// console.log(books[0].title + ' - ' + books[0].author.firstName + ' ' + books[0].author.lastName);
+//     console.log(books[1].title + ' - ' + books[1].author.firstName + ' ' + books[1].author.lastName);
+//     console.log(books[2].title + ' - ' + books[2].author.firstName + ' ' + books[2].author.lastName);
+//     console.log(books[3].title + ' - ' + books[3].author.firstName + ' ' + books[3].author.lastName);
+//     console.log(books[4].title + ' - ' + books[4].author.firstName + ' ' + books[4].author.lastName);
+
+    for(var i = 0; i < books.length; i++){
+        console.log('Book' + books.indexOf());//What
+        console.log('Title: ' + books[i].title);
+    }
     /**
      * TODO:
      * Loop through the books array and output the following information about
