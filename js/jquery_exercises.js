c14ed66cad5070a91958b1f6bec2b33eda774d00
Shelby Polasek <shelbypolasek@gmail.com>
2020-05-12 11:12:55 -0500

    iife wrapped document ready

---
+++ js/jquery_exercises.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(document).ready(function() {
+        alert( 'The DOM has finished loading!' );
+});
+
+//using an iife
+// (function ($) {
+//     $(document).ready(function() {
+//         alert( 'The DOM has finished loading!' );
+//     });
+//
+// })(jQuery);
