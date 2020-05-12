dbb775f8a5729d104fb003549a5875d1b578c3c7
Shelby Polasek <shelbypolasek@gmail.com>
2020-05-12 12:33:11 -0500

    finished jquery intro exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,8 +1,8 @@
 'use strict';
 
-$(document).ready(function() {
-        alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//         alert( 'The DOM has finished loading!' );
+// });
 
 //using an iife
 // (function ($) {
@@ -11,3 +11,15 @@ $(document).ready(function() {
 //     });
 //
 // })(jQuery);
+
+// var heading = $('#heading').html();
+// alert(heading);
+//
+// var musicTitle = $('#listOne').html();
+// alert(musicTitle);
+
+// $('.codeup').css('border', '1px solid red');
+
+$('li').css('font-size', '20px');
+
+$('h1, p, li').css('background-color', 'yellow');
