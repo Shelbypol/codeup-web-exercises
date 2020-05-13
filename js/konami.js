9f6dc094dd5ed2ff6b0dacb1743f4783cc27454e
Shelby Polasek <shelbypolasek@gmail.com>
2020-05-13 17:20:57 -0500

    working on jQuery

--- js/konami.js
+++ js/konami.js
@@ -4,6 +4,13 @@ $(document).keyup(function (event) {
     console.log(event.keyCode);
 });
 
-$('body').keypress(function () {
+$('body').keydown(function () {
     $('#gameboy').css('visibility', 'visible');
+});
+
+
+$('body').on('keydown', function (event) {
+    if (event.key === 'ArrowLeft') {
+        $('#buttonLeft').css('visibility', 'visible');
+    }
 });
