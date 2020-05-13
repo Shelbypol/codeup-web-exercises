540ba6ffac43fb6bccb5110dc7a434aca6dfd093
Shelby Polasek <shelbypolasek@gmail.com>
2020-05-13 10:58:31 -0500

    jQuery event exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -20,6 +20,20 @@
 
 // $('.codeup').css('border', '1px solid red');
 
-$('li').css('font-size', '20px');
+// $('li').css('font-size', '20px');
+//
+// $('h1, p, li').css('background-color', 'yellow');
 
-$('h1, p, li').css('background-color', 'yellow');
+$('h1').click(function () {
+    $(this).css('background', 'teal');
+});
+
+$('p').dblclick(function () {
+    $(this).css('font-size', '18px');
+});
+
+$('li').hover(function () {
+    $(this).css('color', 'red');
+}, function () {
+    $(this).css('color', 'black');
+});
