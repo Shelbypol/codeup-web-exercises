d45433e2da063e9a90cb9dbba5c5d0e28a0e3e0e
Shelby Polasek <shelbypolasek@gmail.com>
2020-05-13 18:55:28 -0500

    adding jQuery functionality

--- js/konami.js
+++ js/konami.js
@@ -9,8 +9,44 @@ $('body').keydown(function () {
 });
 
 
+
+
+setInterval(function () {
+}, 1000 );
+
+    //Arrow Left
+    $('body').on('keydown', function (event) {
+        if (event.key === 'ArrowLeft') {
+            $('#buttonLeft').css('visibility', 'visible');
+        }
+    });
+//Arrow Right
 $('body').on('keydown', function (event) {
-    if (event.key === 'ArrowLeft') {
-        $('#buttonLeft').css('visibility', 'visible');
+    if (event.key === 'ArrowRight') {
+        $('#buttonRight').css('visibility', 'visible');
+    }
+});
+//Arrow Up
+$('body').on('keydown', function (event) {
+    if (event.key === 'ArrowUp') {
+        $('#buttonUp').css('visibility', 'visible');
+    }
+});
+//Arrow Down
+$('body').on('keydown', function (event) {
+    if (event.key === 'ArrowDown') {
+        $('#buttonDown').css('visibility', 'visible');
+    }
+});
+//Button A
+$('body').on('keydown', function (event) {
+    if (event.key === 'a') {
+        $('#buttonA').css('visibility', 'visible');
+    }
+});
+//Button B
+$('body').on('keydown', function (event) {
+    if (event.key === 'b') {
+        $('#buttonB').css('visibility', 'visible');
     }
 });
