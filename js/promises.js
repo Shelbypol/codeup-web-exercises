7b0b64948a5bfc5460d231af94e943f8facbf88e
Shelby Polasek <shelbypolasek@gmail.com>
2020-05-25 16:02:37 -0500

    exercise work

--- js/promises.js
+++ js/promises.js
@@ -1,17 +1,36 @@
-
 // console.log(githubToken);
 
-function githubUserName (username){
-    let githubToken = fetch (`https://api.github.com/users/${username}/events/public` , {headers: {'Authorization': `token ${GITHUB_API}`}});
+function githubUserName(username) {
+    let githubToken = fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `token ${GITHUB_API}`}});
 
     githubToken.then(response => response.json())
-        .then(event => console.log(new Date(event[0].created_at).toDateString()))
-        .catch(error  => console.log(error));
-}
+        // .then(event => console.log(new Date(event[0].created_at).toDateString()))
+        .then(event => {
+            for (var i = 0; i <= 30; i++) {
+             return (event[i].type === 'PushEvent')
+            }
+        })
+        .then(pushEvents => {
+            pushEvents.filter(function (t) {
+                if (t === 'PushEvent') {
+                    console.log('you did it')
+                }
+            })
+        })
+            // .then(type => type.filter(n => n === 'PushEvent'))
 
-githubUserName('Shelbypol');
+            // .then(event =>(event.filter(function (i) {
+            //     console.log(event[i].type. === 'PushEvent');
+            // })))
 
-filter
+            // .then (new Date([0].created_at).toDateString())
+        .
+            catch(error => console.log(error));
+        }
+
+    githubUserName('Shelbypol');
+
+// filter
 
 
 // most recent PushEvent 
